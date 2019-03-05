eb9444e2f3c9c4792bd393d7effaf8cf29711b3d
edwardzhong <edwardloveyou@gmail.com>
2019-03-05 19:29:29 +0800

    update

--- bin/grab.js
+++ bin/grab.js
@@ -1,56 +1,40 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
 const program = require('commander');
-const colors = require('colors');
-const request = require('../lib');
-const log = console.log;
+const { baiduSearch } = require('../lib/baidu');
+const { zhihuSearch } = require('../lib/zhihu');
 
 program
-    // .allowUnknownOption()//不报错误
-    .version('0.0.1')
-    .usage('这是我的网络爬虫程序😎'
-      +'\n  grap [option]'
-      +'\n    bd baidu: baidu search'
-      +'\n    zh zhihu: zhihu search');
+	// .allowUnknownOption()//不报错误
+	.version('0.0.1')
+	.usage('这是我的网络爬虫程序😎'
+		+ '\n  grap [option]'
+		+ '\n    bd baidu: baidu search'
+		+ '\n    zh zhihu: zhihu search')
+	.on('--help', function () {
+		console.log('  Examples:');
+		console.log();
+		console.log('    grab bd    <cmd>');
+		console.log('    grab baidu <cmd>');
+		console.log();
+	});
 
 program
-    .command('baidu <cmd>')
-    .alias('bd')
-    .description('baidu search baidu')
-    .option("-t, --tieba", "baidu tieba")
-    .action(function(cmd, options){
-      console.log('baidu search "%s":', cmd);
-      request.baiduSearch(cmd);
-    }).on('--help', function() {
-      console.log('  Examples:');
-      console.log();
-      console.log('    grab bd    <cmd>');
-      console.log('    grab baidu <cmd>');
-      console.log();
-    });
+	.command('baidu <cmd>')
+	.alias('bd')
+	.description('baidu search')
+	.action(function (cmd, options) {
+		console.log('baidu search "%s":', cmd);
+		baiduSearch(cmd);
+	});
 
 program
-    .command('zhihu <cmd>')
-    .alias('zh')
-    .description('search zhihu')
-    .action(function(cmd, options){
-      console.log('zhihu search "%s":', cmd);
-      request.zhihuSearch(cmd);
-    }).on('--help', function() {
-      console.log('  Examples:');
-      console.log();
-      console.log('    grab zh    <cmd>');
-      console.log('    grab zhihu <cmd>');
-      console.log();
-    });
+	.command('zhihu <cmd>')
+	.alias('zh')
+	.description('search zhihu')
+	.action(function (cmd, options) {
+		console.log('zhihu search "%s":', cmd);
+		zhihuSearch(cmd);
+	});
 
 program.parse(process.argv);
 
-// log(' args: %j', program.args);
-
-if (!process.argv.slice(2).length) {
-  program.outputHelp(make_red);
-}
-
-function make_red(txt) {
-  return colors.red(txt); //display the help text in red on the console
-}
